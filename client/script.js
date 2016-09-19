2734f3a941f72c9196e16317ff58cba848074de4
Jan Maverick Alqueza <alquezajanmaverick@gmail.com>
2016-09-19 10:05:51 +0800

    update files

--- client/script.js
+++ client/script.js
@@ -1,5 +1,8 @@
 'use strict';
 var app = angular.module('CoffeeApp',[]);
 app.controller('CoffeeCtrl',function($scope,$http){
-	
+	$http.get("get-data.php")
+		.then(function(response) {
+			$scope.data = response.data;
+	});
 });
