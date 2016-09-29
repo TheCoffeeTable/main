ccb837dd974e37a3ed04b7a5168c1d68f0941041
Jan Maverick Alqueza <alquezajanmaverick@gmail.com>
2016-09-29 14:31:11 +0800

    sql

--- Coffee_Table.sql
+++ Coffee_Table.sql
@@ -32,7 +32,7 @@ DROP PROCEDURE IF EXISTS `get-items`;
 DELIMITER //
 CREATE DEFINER=`root`@`localhost` PROCEDURE `get-items`()
 BEGIN
-	SELECT * from tbl_items order by categoryname asc;
+	SELECT * from tbl_product order by categoryname asc;
 END//
 DELIMITER ;
 
@@ -60,7 +60,6 @@ CREATE TABLE IF NOT EXISTS `tbl_admin` (
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 -- Dumping data for table the_coffee_table.tbl_admin: 0 rows
-DELETE FROM `tbl_admin`;
 /*!40000 ALTER TABLE `tbl_admin` DISABLE KEYS */;
 /*!40000 ALTER TABLE `tbl_admin` ENABLE KEYS */;
 
@@ -74,37 +73,11 @@ CREATE TABLE IF NOT EXISTS `tbl_category` (
 ) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
 
 -- Dumping data for table the_coffee_table.tbl_category: 2 rows
-DELETE FROM `tbl_category`;
 /*!40000 ALTER TABLE `tbl_category` DISABLE KEYS */;
-INSERT INTO `tbl_category` (`catNo`, `categoryname`) VALUES
+INSERT IGNORE INTO `tbl_category` (`catNo`, `categoryname`) VALUES
 	(1, '123'),
 	(2, 'asd');
 /*!40000 ALTER TABLE `tbl_category` ENABLE KEYS */;
-
-
--- Dumping structure for table the_coffee_table.tbl_items
-DROP TABLE IF EXISTS `tbl_items`;
-CREATE TABLE IF NOT EXISTS `tbl_items` (
-  `itemno` int(11) NOT NULL AUTO_INCREMENT,
-  `status` varchar(50) NOT NULL,
-  `name` varchar(50) NOT NULL,
-  `categoryname` varchar(50) NOT NULL,
-  `description` varchar(50) NOT NULL,
-  `price` decimal(10,2) NOT NULL,
-  `imgURL` varchar(50) NOT NULL,
-  PRIMARY KEY (`itemno`)
-) ENGINE=MyISAM AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
-
--- Dumping data for table the_coffee_table.tbl_items: 5 rows
-DELETE FROM `tbl_items`;
-/*!40000 ALTER TABLE `tbl_items` DISABLE KEYS */;
-INSERT INTO `tbl_items` (`itemno`, `status`, `name`, `categoryname`, `description`, `price`, `imgURL`) VALUES
-	(1, 'AVAILABLE', 'asd', 'asd', 'dsd', 19.00, 'pasta'),
-	(2, 'AVAILABLE', 'qqq', '123', 'dsd', 19.00, 'special'),
-	(3, 'AVAILABLE', 'ghjgh', 'asd', 'dsd', 19.00, 'bananasplit'),
-	(4, 'AVAILABLE', 'ghjgh', '123', 'dsd', 19.00, 'shakes'),
-	(5, 'AVAILABLE', 'ghjgh', '123', 'dsd', 19.00, 'sandwich');
-/*!40000 ALTER TABLE `tbl_items` ENABLE KEYS */;
 
 
 -- Dumping structure for table the_coffee_table.tbl_menu
@@ -121,7 +94,6 @@ CREATE TABLE IF NOT EXISTS `tbl_menu` (
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1 ROW_FORMAT=DYNAMIC;
 
 -- Dumping data for table the_coffee_table.tbl_menu: 0 rows
-DELETE FROM `tbl_menu`;
 /*!40000 ALTER TABLE `tbl_menu` DISABLE KEYS */;
 /*!40000 ALTER TABLE `tbl_menu` ENABLE KEYS */;
 
@@ -130,18 +102,50 @@ DELETE FROM `tbl_menu`;
 DROP TABLE IF EXISTS `tbl_orders`;
 CREATE TABLE IF NOT EXISTS `tbl_orders` (
   `orderNo` int(11) NOT NULL AUTO_INCREMENT,
-  `TableNo` int(11) NOT NULL COMMENT 'kung saang table',
-  `itemno` int(11) NOT NULL,
-  `name` varchar(50) NOT NULL,
-  `qty` int(11) NOT NULL,
-  `price` decimal(10,2) NOT NULL,
+  `TableNo` int(11) DEFAULT NULL COMMENT 'kung saang table',
+  `productNo` varchar(50) DEFAULT NULL,
+  `name` varchar(50) DEFAULT NULL,
+  `qty` int(11) DEFAULT NULL,
+  `price` decimal(10,2) DEFAULT NULL,
   PRIMARY KEY (`orderNo`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
 
--- Dumping data for table the_coffee_table.tbl_orders: 0 rows
-DELETE FROM `tbl_orders`;
+-- Dumping data for table the_coffee_table.tbl_orders: 8 rows
 /*!40000 ALTER TABLE `tbl_orders` DISABLE KEYS */;
+INSERT IGNORE INTO `tbl_orders` (`orderNo`, `TableNo`, `productNo`, `name`, `qty`, `price`) VALUES
+	(1, 1, 'qqq', NULL, 1, 19.00),
+	(2, 1, 'asd', NULL, 1, 19.00),
+	(3, 1, '2', NULL, 1, 19.00),
+	(4, 1, '4', NULL, 1, 19.00),
+	(5, 1, 'qqq', '2', 1, 19.00),
+	(6, 1, 'ghjgh', '4', 1, 19.00),
+	(7, 1, 'ghjgh', '5', 1, 19.00),
+	(8, 1, 'asd', '1', 1, 19.00);
 /*!40000 ALTER TABLE `tbl_orders` ENABLE KEYS */;
+
+
+-- Dumping structure for table the_coffee_table.tbl_product
+DROP TABLE IF EXISTS `tbl_product`;
+CREATE TABLE IF NOT EXISTS `tbl_product` (
+  `itemno` int(11) NOT NULL AUTO_INCREMENT,
+  `status` varchar(50) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `categoryname` varchar(50) NOT NULL,
+  `description` varchar(50) NOT NULL,
+  `price` decimal(10,2) NOT NULL,
+  `imgURL` varchar(50) NOT NULL,
+  PRIMARY KEY (`itemno`)
+) ENGINE=MyISAM AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+
+-- Dumping data for table the_coffee_table.tbl_product: 5 rows
+/*!40000 ALTER TABLE `tbl_product` DISABLE KEYS */;
+INSERT IGNORE INTO `tbl_product` (`itemno`, `status`, `name`, `categoryname`, `description`, `price`, `imgURL`) VALUES
+	(1, 'AVAILABLE', 'asd', 'asd', 'dsd', 19.00, 'pasta'),
+	(2, 'AVAILABLE', 'qqq', '123', 'dsd', 19.00, 'special'),
+	(3, 'AVAILABLE', 'ghjgh', 'asd', 'dsd', 19.00, 'bananasplit'),
+	(4, 'AVAILABLE', 'ghjgh', '123', 'dsd', 19.00, 'shakes'),
+	(5, 'AVAILABLE', 'ghjgh', '123', 'dsd', 19.00, 'sandwich');
+/*!40000 ALTER TABLE `tbl_product` ENABLE KEYS */;
 
 
 -- Dumping structure for table the_coffee_table.tbl_staff_account
@@ -160,7 +164,6 @@ CREATE TABLE IF NOT EXISTS `tbl_staff_account` (
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 -- Dumping data for table the_coffee_table.tbl_staff_account: 0 rows
-DELETE FROM `tbl_staff_account`;
 /*!40000 ALTER TABLE `tbl_staff_account` DISABLE KEYS */;
 /*!40000 ALTER TABLE `tbl_staff_account` ENABLE KEYS */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
