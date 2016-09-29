f2d04c2900874d523f33b80724648856d98db95a
Jan Maverick Alqueza <alquezajanmaverick@gmail.com>
2016-09-29 15:33:36 +0800

    update

--- client/i-order.php
+++ client/i-order.php
@@ -7,8 +7,8 @@ foreach($request as $r)
 {
 	$db->query("INSERT into tbl_orders(`TableNo`,`productNo`,`name`,`qty`,`price`)VALUES(?,?,?,?,?)");
 	$db->bind(1,$r->tblno);
-	$db->bind(2,$r->name);
-	$db->bind(3,$r->itemno);
+	$db->bind(2,$r->itemno);
+	$db->bind(3,$r->name);
 	$db->bind(4,$r->qty);
 	$db->bind(5,$r->price);
 	$db->execute();	
